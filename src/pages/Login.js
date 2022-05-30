1c9f5efb5bb32b65093dc8c408d6d20ed92fa264
권우민 <34460348+yvsc5020@users.noreply.github.com>
2022-05-30 10:59:43 +0900

    [0530] 셈플 파일 정리

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function Login() {
-  return (
-    <div>
-      <LoginForm />
-      <Other />
-      <LoginFooter />
-    </div>
-  )
+  return <></>;
 }
 
-export default Login
+export default Login;
