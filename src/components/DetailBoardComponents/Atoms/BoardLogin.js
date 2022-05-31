a9c6b784a2ac424c7938b375c85395936a37b7ba
jaewon lee <jwleetop5@naver.com>
2022-05-31 20:33:04 +0900

    [0531] DetailBoard header 거의완성

--- src/components/DetailBoardComponents/Atoms/BoardLogin.js
+++ src/components/DetailBoardComponents/Atoms/BoardLogin.js
@@ -1,17 +1,38 @@
 import React from "react";
 import GlobalStyle from "../GlobalStyle";
 import styled from "styled-components";
+import svg from "../../../images/Ellipse 3.png";
 function BoardLogin() {
   return (
     <>
       <GlobalStyle />
-      <MyName>판매자</MyName>
-      <YouRating>열심회원</YouRating>
-      <BuyQuestion>구매문의</BuyQuestion>
-      <UpLoaddate>2022.05.04 00:27 조회 5</UpLoaddate>
+      <Wrapper>
+        <DogImg src={svg}></DogImg>
+        <div>
+          <MyName>판매자</MyName>
+          <YouRating>열심회원</YouRating>
+          <BuyQuestion>구매문의</BuyQuestion>
+          <UpLoaddate>2022.05.04 00:27 조회 5</UpLoaddate>
+        </div>
+      </Wrapper>
+      <URLCopy>URL 복사</URLCopy>
+      <Comment>
+        댓글 <CommentCnt>0 </CommentCnt>
+      </Comment>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const DogImg = styled.img`
+  width: 3rem;
+  height: 3rem;
+  margin-right: 10px;
+  border-radius: 50%;
+`;
 const MyName = styled.div`
   font-size: 14pt;
   font-weight: bold;
@@ -32,14 +53,33 @@ const UpLoaddate = styled.div`
   color: #999999;
   padding-left: 10px;
 `;
-const BuyQuestion = styled.div`
+const BuyQuestion = styled.button`
   font-size: 13pt;
   display: inline-block;
   font-weight: bold;
-  padding: 5px 15px;
+  padding: 5px 12px;
+  border: 0;
+  cursor: pointer;
   text-align: center;
   background-color: #dbdbdb;
   border-radius: 10px;
   margin-left: 7px;
+`;
+
+const Comment = styled.div`
+  font-size: 13pt;
+  display: block;
+  float: right;
+  margin-right: 10px;
+`;
+const CommentCnt = styled.div`
+  font-size: 13pt;
+  display: inline-block;
+  font-weight: bold;
+`;
+const URLCopy = styled.div`
+  display: block;
+  font-size: 13pt;
+  float: right;
 `;
 export default BoardLogin;
