51a01e3b1e4fb3286e76a5c5298f69e122d4de15
jaewon lee <jwleetop5@naver.com>
2022-05-31 11:01:45 +0900

    Update GradeTitle.js

--- src/components/GradeBoardComponents/Atoms/GradeTitle.js
+++ src/components/GradeBoardComponents/Atoms/GradeTitle.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import styled from "styled-components";
+import GlobalStyle from "../GlobalStyle";
 function GradeTitle() {
-  return <div>gradeTitle</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <PinkSpan>카페의 회원 등급</PinkSpan>
+    </>
+  );
 }
-
+const PinkSpan = styled.span`
+  font-size: 13pt;
+  font-weight: bold;
+  display: inline-block;
+  color: #ff50e2;
+`;
 export default GradeTitle;
