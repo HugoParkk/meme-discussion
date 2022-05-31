0563feb01bfe8021daaef7df89055fbd0f2fd030
jaewon lee <jwleetop5@naver.com>
2022-05-31 20:52:53 +0900

    [0531] BoardMainText - 1완성

---
+++ src/components/DetailBoardComponents/Atoms/BoardMainText.js
@@ -0,0 +1,26 @@
+import React from "react";
+import GlobalStyle from "../GlobalStyle";
+import styled from "styled-components";
+function BoardMainText() {
+  return (
+    <>
+      <GlobalStyle />
+      <Sell>판매</Sell>
+      <SellText>트와이스 나연 i Love Paris짤 팝니다</SellText>
+    </>
+  );
+}
+
+const Sell = styled.div`
+  font-size: 13pt;
+  color: #ff50e2;
+  font-weight: bold;
+  margin-right: 10px;
+  display: inline-block;
+`;
+const SellText = styled.div`
+  font-size: 18pt;
+  display: inline-block;
+  font-weight: bold;
+`;
+export default BoardMainText;
