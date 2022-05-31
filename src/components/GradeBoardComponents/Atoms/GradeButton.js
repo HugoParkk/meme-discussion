01072a6073244cffb7db2ed96ee589145e106e75
권우민 <34460348+yvsc5020@users.noreply.github.com>
2022-05-31 11:02:04 +0900

    [0531] GradeButton 완성, GradeTextGroup 정의

--- src/components/GradeBoardComponents/Atoms/GradeButton.js
+++ src/components/GradeBoardComponents/Atoms/GradeButton.js
@@ -1,11 +1,39 @@
 import React from "react";
+import styled from "styled-components";
 
-function GradeButton() {
+function GradeButton(props) {
   return (
     <>
-      <div>gradeButton</div>
+      {props.type === "white" ? (
+        <WhiteDiv>{props.text}</WhiteDiv>
+      ) : (
+        <PinkDiv>{props.text}</PinkDiv>
+      )}
     </>
   );
 }
+
+const WhiteDiv = styled.div`
+  width: 120px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  background-color: white;
+  border: 1px solid #cfcfcf;
+  display: inline-block;
+  margin-right: 15px;
+  border-radius: 5px;
+`;
+
+const PinkDiv = styled.div`
+  width: 120px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  background-color: #ff50e2;
+  display: inline-block;
+  color: white;
+  border-radius: 5px;
+`;
 
 export default GradeButton;
