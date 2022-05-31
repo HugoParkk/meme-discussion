4a99db6b62b417902d50d6e06cfca22911e5eaba
박종연 <20041230andy@gmail.com>
2022-05-31 18:24:58 +0900

    [0531] React router dom added
    react route added

--- src/components/GradeBoardComponents/Atoms/GradeText.js
+++ src/components/GradeBoardComponents/Atoms/GradeText.js
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import GlobalStyle from "../GlobalStyle";
 
-function GradeText() {
-  return <div>gradeText</div>;
+function GradeText(props) {
+  return (
+    <>
+      <GlobalStyle />
+      {props.type === "pink" ? (
+        <PinkDiv>{props.text}</PinkDiv>
+      ) : (
+        <DefaultDiv>{props.text}</DefaultDiv>
+      )}
+    </>
+  );
 }
+
+const PinkDiv = styled.div`
+  color: white;
+  background-color: #ff50e2;
+  display: inline-block;
+  clear: both;
+  padding: 2px;
+  padding-left: 6px;
+  padding-right: 6px;
+  border-radius: 7px;
+  font-size: 13px;
+`;
+
+const DefaultDiv = styled.div`
+  color: black;
+  display: inline-block;
+  clear: both;
+  padding: 2px;
+  padding-left: 6px;
+  padding-right: 6px;
+  font-size: 13px;
+`;
 
 export default GradeText;
