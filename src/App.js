212ac29c090cd4bed13f4e18a302249e15bc4c4d
박종연 <20041230andy@gmail.com>
2022-05-31 16:39:18 +0900

    [0531] App.js Login test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './pages/Main';
+import Login from './pages/Login'
 
 // react-responsive(반응형 웹을 만들기 위한 모듈 테스트)
 import { useMediaQuery } from 'react-responsive'
@@ -18,7 +18,8 @@ function App() {
 
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <Login />
     </div>
   );
 }
