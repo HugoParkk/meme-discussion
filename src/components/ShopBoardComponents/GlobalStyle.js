a6df2740927c26fc2acfb6a502ff95ce57216331
jaewon lee <jwleetop5@naver.com>
2022-06-04 23:22:37 +0900

    [0604] GlobalStyle

---
+++ src/components/ShopBoardComponents/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing : border-box;
+  }
+  body{
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
