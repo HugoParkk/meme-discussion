fdaf67dee427276342403f6c33a0303a96e5e9d2
류채연 <haha03270@gmail.com>
2022-06-09 20:20:32 +0900

    [0609] 이게뭐지

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,51 @@
 import React from 'react'
+<<<<<<< Updated upstream
+=======
+import styled from 'styled-components';
+
+import bookmarkStar from './images/public_compoment/star_sideMenu.svg';
+import profile from './images/public_compoment/profile.svg';
+import crown from './images/public_compoment/crown.png';
+
+
+>>>>>>> Stashed changes
 
 function Sidebar() {
   return (
     <div>
+<<<<<<< Updated upstream
       
+=======
+      <SideMenuBar>
+        <div id="UpSideMenu">
+          <div id="head">
+          <div id="BookMarkIcon">
+            <img src={bookmarkStar} alt="북마크아이콘"></img>
+          </div>
+          <div id="tab-title">
+            <ul> 
+              <li>카페정보</li>
+              <li>나의활동</li>
+            </ul>
+          </div>
+          
+          <div id="Userinfo"></div>
+            <div id="Userprofile">
+              <img src={profile} alt="프로필이미지"></img>
+              <span id="gradeBox">매니저</span>주인짱
+              <ul id="profile-info">
+                <li>since 2002.02.02.</li>
+                <li>카페소개</li>
+              </ul>
+            </div>
+            <div id="CafeInfo">
+              <div id="Queen"><img src={crown} alt="Queen아이콘"></img>퀸</div>
+
+            </div>
+          </div>
+        </div>
+      </SideMenuBar>
+>>>>>>> Stashed changes
     </div>
   )
 }
