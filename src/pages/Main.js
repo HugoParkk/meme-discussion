f3bacae4429b90adea4aad85fd0d84356b11500d
presiti <zinna0347@gmail.com>
2022-06-10 18:13:09 +0900

    [0610] 이게머냐고

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,12 +5,19 @@ import Header from '../components/Header';
 import Popup from '../components/Popup';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
+import SellingImg from "../images/Vector.png"
 
 function Main() {
   return (
     <div>
       <Warp>
         <Header />
+        <MainTop>
+          <div id="plsSell">
+            <img src={SellingImg} alt="이미지팔아드릴게"></img>
+          </div>
+          <div id="Notice-Update"></div>
+        </MainTop>
         {/* <div id="main-popup"><Popup /></div> */}
         {/* <div id="main-sidebar"><Sidebar /></div> */}
         <Footer />
@@ -31,6 +38,10 @@ margin: 0 auto;
   position: absolute;
   left: 0;
 }
+`
+
+const MainTop = styled.div`
+
 `
 
 export default Main
