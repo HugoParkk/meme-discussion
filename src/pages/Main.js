6d419b95100a31f056b863aa4d8d80d6f39480c1
jaewon lee <jwleetop5@naver.com>
2022-06-15 21:32:25 +0900

    [0615] 잡다한것들은 완성되었다..

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import Header from '../components/Header';
+import Header from "../components/Header";
 // import Popup from '../components/Popup';
-import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
 
 //----images----
 
-import twinkle from '../images/main_page/twinkle_bg_c.png';
-import Card from '../images/main_page/Card_a.png';
-import plus_box from '../images/main_page/plus_box.png';
+import twinkle from "../images/main_page/twinkle_bg_c.png";
+import Card from "../images/main_page/Card_a.png";
+import plus_box from "../images/main_page/plus_box.png";
 
-import JJalSell from '../images/main_page/JJal_sellMain.png'
+import JJalSell from "../images/main_page/JJal_sellMain.png";
+import SideBarSnsUploadText from "../components/UploadBoardComponents/Atoms/SideBarSnsUploadText";
+import SideBarUpload from "../components/UploadBoardComponents/Molecules/SideBarUpload";
 
 function Main() {
   return (
     <div>
-      <Warp>
+      {/* <Warp>
       <Header/>
         <div id="public-sidebar"><Sidebar /></div>
         <div id = "main-contents">
@@ -63,13 +65,14 @@ function Main() {
         </div>
           </MainMiddle>
 
-          </div>
-          {/* <div id="main-popup"><Popup /></div> */}
+          </div> */}
+      {/* <div id="main-popup"><Popup /></div> */}
 
-    {/* <div id="main-popup"><Popup /></div> */ }
-    <Footer id="public-footer"/>
-      </Warp >
-    </div >
+      {/* <div id="main-popup"><Popup /></div> */}
+      {/* <Footer id="public-footer"/>
+      </Warp > */}
+      <SideBarUpload />
+    </div>
   );
 }
 
@@ -78,119 +81,117 @@ const MainMiddle = styled.div`
   margin: 20px auto;
   padding: 0;
   position: relative;
-  #best_JJal{
+  #best_JJal {
     width: 670px;
     height: 424px;
     background: url(${twinkle});
-    
+
     margin: 0 0 14px 0;
     border: 4px solid #ccc;
     outline: 2px solid #000;
-    #text{
+    #text {
       font-size: 18px;
 
       /* border-bottom: 2px solid #cfcfcf; */
       padding: 10px;
       margin-left: 10px;
-      span{
+      span {
         font-size: 12px;
         font-weight: bold;
         margin-left: 325px;
       }
     }
-    .card{
-    margin: auto -8px;
-    img{
-      width: 238px;
-      height: 342px;
+    .card {
+      margin: auto -8px;
+      img {
+        width: 238px;
+        height: 342px;
+      }
     }
+    .card_set {
+      display: flex;
+      justify-content: center;
+      /* top: 50px; */
+      /* position: relative; */
 
+      .center {
+        margin-bottom: 60px;
+      }
+    }
   }
-  .card_set{
-    display: flex;
-    justify-content: center;
-    /* top: 50px; */
-    /* position: relative; */
-    
-    .center{
-      margin-bottom: 60px;
-        }
-    
-  } 
-  
-  }
-  
-  #Rank{
+
+  #Rank {
     width: 172px;
     height: 432px;
     background: magenta;
   }
-`
+`;
 
 const MainTop = styled.div`
-width: 100%;
-height: 428px;
-position: relative;
+  width: 100%;
+  height: 428px;
+  position: relative;
 
-#sell_JJal{
-width: 492px;
-height: 100%;
-position: absolute;
-left: 0;
-background: #CFCFCF;
+  #sell_JJal {
+    width: 492px;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    background: #cfcfcf;
 
-  img{
-    padding: 5px 0 0 5px;
+    img {
+      padding: 5px 0 0 5px;
+    }
   }
-}
 
-#notice_update{
-  width: 338px;
-  height: 100%;
-  position: absolute;
-  right: 0;
-  background: #FF05E2;
-  #listEmptySpace{
-    height: 100px;
+  #notice_update {
+    width: 338px;
+    height: 100%;
+    position: absolute;
+    right: 0;
+    background: #ff05e2;
+    #listEmptySpace {
+      height: 100px;
 
-    ul{
-      position: absolute;
-      list-style: none; left:-10px;
-      li{
-        padding: 5px 0;
+      ul {
+        position: absolute;
+        list-style: none;
+        left: -10px;
+        li {
+          padding: 5px 0;
+        }
       }
     }
   }
-}
-`
+`;
 
 const Warp = styled.div`
-/*여기에 public의 코드는 공통 컴포넌트로 넘길 생각 중 */
-position: relative;
-width: 1080px;
-margin: 0 auto;
-/* min-height: 100vh; */
-
-#public-sidebar{
-  position: absolute;
-  left: 0;
-}
-
-#public-popup{
-  position: absolute;
-  margin: 0 100px;
-}
-
-#main-contents{
+  /*여기에 public의 코드는 공통 컴포넌트로 넘길 생각 중 */
   position: relative;
-  width: 850px;
-  top: 37.5px;
-  left: 230px;
-}
+  width: 1080px;
+  margin: 0 auto;
+  /* min-height: 100vh; */
 
-#public-footer{
-  position: absolute;
-}
-`
+  #public-sidebar {
+    position: absolute;
+    left: 0;
+  }
+
+  #public-popup {
+    position: absolute;
+    margin: 0 100px;
+  }
+
+  #main-contents {
+    position: relative;
+    width: 850px;
+    top: 37.5px;
+    left: 230px;
+  }
+
+  #public-footer {
+    position: absolute;
+  }
+`;
 
 export default Main;
