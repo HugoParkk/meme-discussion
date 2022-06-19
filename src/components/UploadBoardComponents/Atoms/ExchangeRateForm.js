515f4e2e5dbf173993e49d4de3261c89fd9a4b70
jaewon lee <jwleetop5@naver.com>
2022-06-19 22:10:08 +0900

    [0619] RightBar 완성..? 기능은 입력받기 밖에 없다..

---
+++ src/components/UploadBoardComponents/Atoms/ExchangeRateForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import GlobalStyle from "../GlobalStyle";
+function ExchangeRateForm() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <YourForm type="text" placeholder="130" />
+      </div>
+    </>
+  );
+}
+const YourForm = styled.input`
+  box-shadow: 1px 1px 10px #ff50e2;
+  width: 160px;
+  height: 28px;
+  border: 1.5px solid black;
+  border-radius: 5px;
+  color: #ff50e2;
+  font-size: 13pt;
+  text-align: right;
+  padding: 10px;
+  ::placeholder {
+    color: #ff50e2;
+    font-size: 13pt;
+    text-align: right;
+    font-weight: bold;
+  }
+`;
+
+export default ExchangeRateForm;
