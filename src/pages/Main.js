05c34c198146c352204bd132b7ac11d6adab21e1
presiti <zinna0347@gmail.com>
2022-06-21 00:32:44 +0900

    [0621]Sidebar-Topbox-Finish

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -47,20 +47,31 @@ function Main() {
             <div id="sell_JJal">
               <img src={JJalSell}></img>
             </div>
-            <div id="notice_update">
-              <div class="title">공지 및 업데이트<sapn>더보기 <img src={plus_box}></img></sapn></div>
-              <div id="listEmptySpace">
-                <ul>
-                  <li><button>공지</button> 출석미션 보상받기 오류 해결 완료되...&#91;6&#93;<span class="ViewNum">209</span></li>
-                  <li><button>공지</button> 출석미션 보상받기 오류 해결 완료되...&#91;6&#93;<span class="ViewNum">209</span></li>
-                  <li><button>공지</button> 출석미션 보상받기 오류 해결 완료되...&#91;6&#93;<span class="ViewNum">209</span></li>
-                </ul>
+            <div id="noticeUpdate">
+              <div class="noticeUpdate">
+                <div id="noticeTitle">
+                  <ul>
+                    <li><a class="noticeText">공지 및 업데이트</a></li>
+                    <li><a class="noticePlus">더보기&nbsp;&nbsp;&nbsp;<img src={Plus}></img></a></li>
+                  </ul>
+                </div>
+                <div id="noticeContent">
+                  <div id="listEmptySpace">
+                    <ul>
+                      <li><button>공지</button>&nbsp;&nbsp;&nbsp;<a>출석미션 보상받기 오류 해결 완료되...&#91;6&#93;</a><span>209</span></li>
+                      <li><button>공지</button>&nbsp;&nbsp;&nbsp;<a>출석미션 보상받기 오류 해결 완료되...&#91;6&#93;</a><span>209</span></li>
+                      <li><button>공지</button>&nbsp;&nbsp;&nbsp;<a>출석미션 보상받기 오류 해결 완료되...&#91;6&#93;</a><span>209</span></li>
+                    </ul>
+                  </div>
+                  <div id="listBasic">
+                    <ul>
+                      <li><a>출석미션 보상받기 오류 해결 완료되...</a><span class="YellowText">&#91;6&#93;</span><span class="ViewNum">209</span></li>
+                      <li><a>출석미션 보상받기 오류 해결 완료되...</a><span class="YellowText">&#91;6&#93;</span><span class="ViewNum">209</span></li>
+                      <li><a>출석미션 보상받기 오류 해결 완료되...</a><span class="YellowText">&#91;6&#93;</span><span class="ViewNum">209</span></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-              <ul>
-                <li>출석미션 보상받기 오류 해결 완료되...<span class="YellowText">&#91;6&#93;</span><span class="ViewNum">209</span></li>
-                <li>출석미션 보상받기 오류 해결 완료되...<span class="YellowText">&#91;6&#93;</span><span class="ViewNum">209</span></li>
-                <li>출석미션 보상받기 오류 해결 완료되...<span class="YellowText">&#91;6&#93;</span><span class="ViewNum">209</span></li>
-              </ul>
             </div>
           </MainTop>
 
@@ -441,48 +452,100 @@ const MainTop = styled.div`
     position: absolute;
     left: 0;
     background: #cfcfcf;
+    border: 2px solid #000;
 
     img {
       padding: 5px 0 0 5px;
     }
   }
 
-  #notice_update {
-    width: 360px;
-    height: 100%;
+  #noticeUpdate {
+    width: 354px;
+    height: 420px;
     position: absolute;
     right: 0;
-    background: #ff05e2;
+    background: #fA00D0;
     color: #FFFFFF;
+    border: 4px solid #FF50E2;
 
-    #title{
-      font-size: 18px;
-      font-weight: 600;
-      span{
-        font-size: 12px;
-      }
-    }
-
-    #listEmptySpace {
-      height: 100px;
-
-      ul {
-        position: absolute;
-        list-style: none;
-        left: -18px;
-        li {
-          button {
-            font-size: 12px;
-          }
+    ul{margin: 0; padding:0;}
+    
+      .noticeUpdate{
+          #noticeTitle{
+            width: 355px;
+            height: 40px;
+            border-top: 2px solid #FF50E2;
+            border-bottom: 2px solid #FF50E2;
+            
+            ul{
+              list-style: none;
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+              margin: 0;
+              padding: 8px 15px;
+            }
+            .noticeText{
+              font-size: 18px;
+              font-weight: 600;
+            }
+            .noticePlus{
+              font-size: 12px;
+            }
         }
-      }
-    }
 
-    ul{
-      left: -7px;
-      li{
-        font-size: 13px;
-        padding: 5px 0;
+        #noticeContent{
+          li{
+            font-size: 13px;
+            padding: 15px 0;
+            border-bottom: 1px solid #FF50E2;
+            .YellowText{
+              color: #FAFF00;
+            }
+            .ViewNum{
+              margin-left: 60px;
+            }
+          }
+
+          #listEmptySpace {
+          width: 100%;
+          height: 160px;
+        
+            ul {
+               position: absolute;
+                list-style: none;
+                left: -18px;
+                padding-left: 18px;
+              li {
+                width: 345px;
+                padding-left: 10px;
+                span{margin-left: 20px;}
+                button {
+                  font-size: 12px;
+                  background-color: #F7F9BD;
+                  border: 1px solid #000;
+                  border-radius: 3px;
+                  padding: 2px 11px;
+                }
+              }
+            }
+          }
+
+          #listBasic{
+            width: 100%;
+            height: 160px;  
+            ul{
+              list-style-position: inside;
+
+              li{
+              padding-left: 10px;
+
+                .YellowText{color: #FAFF00;}
+                .ViewNum{margin-left: 60px; }
+              }
+            }
+          }
+
         }
       }
     }
