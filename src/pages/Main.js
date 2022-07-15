5c93b64d88f19a1d7f9c71c8ac84e45907a894f9
presiti <zinna0347@gmail.com>
2022-07-15 19:26:58 +0900

    [0715] 아잉 집에 가고 싶은데

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -37,7 +37,7 @@ import mimgi from "../images/main_page/mimgi.png";
 function Main() {
   return (
     <div>
-      <Warp>
+      <Wrap>
         <Header />
         <Assembler>
           <Sidebar id="public-sidebar" />
@@ -55,9 +55,9 @@ function Main() {
                         <a class="noticeText">공지 및 업데이트</a>
                       </li>
                       <li>
-                        <a class="noticePlus">
-                          더보기&nbsp;&nbsp;&nbsp;<img src={Plus}></img>
-                        </a>
+                        <div class="noticePlus">
+                          더보기&nbsp;&nbsp;<img src={Plus}></img>
+                        </div>
                       </li>
                     </ul>
                   </div>
@@ -115,9 +115,7 @@ function Main() {
               <div id="best_JJal">
                 <div id="text">
                   BEST ZZAL : 내가 제일 '짤'나가{" "}
-                  <span>
-                    더보기 <img src={plus_box}></img>
-                  </span>
+                  <span>더보기 </span><img src={plus_box}></img>
                 </div>
                 <div className="card_set">
                   <div className="card">
@@ -396,7 +394,7 @@ function Main() {
         </div> */}
 
         <Footer id="public-footer" />
-      </Warp>
+      </Wrap>
     </div>
   );
 }
@@ -409,7 +407,7 @@ const Assembler = styled.div`
   margin-top: 40px;
 `;
 
-const Warp = styled.div`
+const Wrap = styled.div`
   position: relative;
   width: 1080px;
   margin: 0 auto;
@@ -481,6 +479,10 @@ const MainTop = styled.div`
         }
         .noticePlus {
           font-size: 12px;
+          display: flex;
+          width: 58px;
+          justify-content: space-between;
+          align-items: center;
         }
       }
 
@@ -565,13 +567,12 @@ const MainMiddle = styled.div`
     outline: 2px solid #000;
     #text {
       font-size: 18px;
-
+      display: flex;
       padding: 10px;
       margin-left: 10px;
       span {
         font-size: 12px;
         font-weight: bold;
-        margin-left: 325px;
       }
     }
     .card {
