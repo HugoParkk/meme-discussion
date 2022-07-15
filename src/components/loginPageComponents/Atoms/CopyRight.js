c4d3d2ce821296cabb09de459bd542e18ebcd615
류채연 <haha03270@gmail.com>
2022-07-15 12:08:37 +0900

    [0715] 로그인 페이지 수정 완료
    긴 한데 백그라운드 수정할 필요 있어보임

--- src/components/loginPageComponents/Atoms/CopyRight.js
+++ src/components/loginPageComponents/Atoms/CopyRight.js
@@ -1,26 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import GlobalStyle from "../GlobalStyle";
+import mini_title from "../img/mini_title.png";
 
 function CopyRight() {
   return (
     <>
       <GlobalStyle />
       <StyledDiv>
-        <StyledText>밈품명품</StyledText> CopyRight for Channel A.All Right
-        Reserved
+      <img src={mini_title} alt="밈품명품" id="mini_title"></img>
+        <StyledText>CopyRight for Channel A.All Right
+Reserved</StyledText>
+        
       </StyledDiv>
     </>
   );
 }
 
 const StyledText = styled.div`
-  font-weight: bold;
   display: inline-block;
-  font-size: 13.5px;
+  padding-left: 8px;
 `;
 
 const StyledDiv = styled.div`
+  position: relative;
+  #mini_title {
+    position: relative;
+    top: 3px;
+  }
   font-size: 12px;
   text-align: center;
   color: white;
